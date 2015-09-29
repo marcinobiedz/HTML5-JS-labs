aaedf84963eb15102bf3dd1b55a905cab89945a8
marcinobiedz <marcin.obiedz@gmail.com>
2015-09-29 17:03:13 +0200

    All files deleted.

--- LABangular2/http-server.cmd
+++
@@ -1,5 +0,0 @@
-@IF EXIST "%~dp0\node.exe" (
-  "%~dp0\node.exe" "%~dp0\.\node_modules\http-server\bin\http-server" %*
-) ELSE (
-  node "%~dp0\.\node_modules\http-server\bin\http-server" %*
-)
--- LABangular2/karma.cmd
+++
@@ -1,5 +0,0 @@
-@IF EXIST "%~dp0\node.exe" (
-  "%~dp0\node.exe" "%~dp0\.\node_modules\karma\bin\karma" %*
-) ELSE (
-  node "%~dp0\.\node_modules\karma\bin\karma" %*
-)
